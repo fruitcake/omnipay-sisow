f0ef8506b4ac1b77ad49399d64ac8d265880c32e
Barry vd. Heuvel <barryvdh@gmail.com>
2015-05-20 13:56:06 +0200

    Add meta file

---
+++ .phpstorm.meta.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace PHPSTORM_META {
+
+    /** @noinspection PhpIllegalArrayKeyTypeInspection */
+    /** @noinspection PhpUnusedLocalVariableInspection */
+    $STATIC_METHOD_TYPES = [
+      \Omnipay\Omnipay::create('') => [
+        'Sisow' instanceof \Omnipay\Sisow\Gateway,
+      ],
+      \Omnipay\Common\GatewayFactory::create('') => [
+        'Sisow' instanceof \Omnipay\Sisow\Gateway,
+      ],
+    ];
+}
